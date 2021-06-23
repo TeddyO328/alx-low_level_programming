31981669d532d858351e18d2b7ab826488cb31aa
TeddyO328 <omosh60@gmail.com>
2021-06-23 10:45:40 +0300

    nested loops

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,26 +1,29 @@
+#include "holberton.h"
 #include <stdio.h>
 
 /**
- * print_to_98 - prints every number from n to 98
- * @n: starting number
+ * print_to_98 - prints all the natural numbers from n to 98
+ *
+ * @n: interger to be tested
+ * Return: list of numbers
  */
 void print_to_98(int n)
 {
-int i;
+	int i;
 
-if (n <= 98)
-{
-for (i = n; i < 98; i++)
-{
-printf("%i, ", i);
-}
-}
-else
-{
-for (i = n; i > 98; i--)
-{
-printf("%i, ", i);
-}
-}
-printf("98\n");
+	if (n <= 98)
+	{
+		for (i = n; i < 98; i++)
+		{
+			printf("%i, ", i);
+		}
+	}
+	else
+	{
+		for (i = n; i > 98; i--)
+		{
+			printf("%i, ", i);
+		}
+	}
+	printf("98\n");
 }
