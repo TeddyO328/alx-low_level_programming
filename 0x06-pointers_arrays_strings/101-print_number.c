c22c192c5c3f3a58e2c545965162794b19f970d2
TeddyO328 <omosh60@gmail.com>
2021-07-01 00:13:07 +0300

    C - More pointers, arrays and strings

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,18 +1,21 @@
 #include "holberton.h"
 
 /**
- * print_number - prints an interger.
- * @n: interger.
- * Return: nothing.
+ * print_number -print number putchar
+ * @n:integer
+ * Return: void
  */
 void print_number(int n)
 {
+	unsigned int x = n;
+
 	if (n < 0)
 	{
 		_putchar('-');
-		n = -n;
+		x = -x;
 	}
-	if (n / 10)
-		print_number(n / 10);
-	_putchar('0' + n % 10);
+	if ((x / 10) > 0)
+		print_number(x / 10);
+
+	_putchar(x % 10 + '0');
 }
